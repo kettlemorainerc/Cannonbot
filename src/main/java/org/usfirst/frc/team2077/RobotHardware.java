365dd98548544c1038411a9f6390d96e06a8ba63
AJ Memmel <averyparanoiduser@gmail.com>
2022-06-06 17:32:17 -0500

    attempting push again from last commit

--- src/main/java/org/usfirst/frc/team2077/RobotHardware.java
+++ src/main/java/org/usfirst/frc/team2077/RobotHardware.java
@@ -11,20 +11,26 @@ public class RobotHardware implements org.usfirst.frc.team2077.common.RobotHardw
     private final AbstractChassis CHASSIS;
 
 
+    public final RotationMotor northEast = new RotationMotor(RotationMotor.MotorPosition.FRONT_RIGHT);
+    public final RotationMotor northWest = new RotationMotor(RotationMotor.MotorPosition.FRONT_LEFT);
+    public final RotationMotor southEast = new RotationMotor(RotationMotor.MotorPosition.BACK_RIGHT);
+    public final RotationMotor southWest = new RotationMotor(RotationMotor.MotorPosition.BACK_LEFT);
+
+
     public final CANLineSubsystem.SparkNeo FRONT_LEFT_WHEEL = new CANLineSubsystem.SparkNeo(SparkNeoDriveModule.DrivePosition.FRONT_LEFT);
 //    public final CANLineSubsystem.SwerveModule NORTH_EAST_MODULE = new CANLineSubsystem.SwerveModule(SparkNeoDriveModule.DrivePosition.NORTH_EAST_SWERVE_MODULE);
-    public final  = new SwerveModule(RobotMap.magnitudeMotor1, RobotMap.directionMotor1, RobotMap.encoder1ChannelA, RobotMap.encoder1ChannelB, length/2, width/2);
-        northEast.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
-
-
-    southEast = new SwerveModule(RobotMap.magnitudeMotor2, RobotMap.directionMotor2, RobotMap.encoder2ChannelA, RobotMap.encoder2ChannelB, -length/2, width/2);
-        southEast.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
-
-    southWest = new SwerveModule(RobotMap.magnitudeMotor3, RobotMap.directionMotor3, RobotMap.encoder3ChannelA, RobotMap.encoder3ChannelB, -length/2, -width/2);
-        southWest.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
-
-    northWest = new SwerveModule(RobotMap.magnitudeMotor4, RobotMap.directionMotor4, RobotMap.encoder4ChannelA, RobotMap.encoder4ChannelB, length/2, -width/2);
-        northWest.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
+//    public final  = new SwerveModule(RobotMap.magnitudeMotor1, RobotMap.directionMotor1, RobotMap.encoder1ChannelA, RobotMap.encoder1ChannelB, length/2, width/2);
+//        northEast.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
+//
+//
+//    southEast = new SwerveModule(RobotMap.magnitudeMotor2, RobotMap.directionMotor2, RobotMap.encoder2ChannelA, RobotMap.encoder2ChannelB, -length/2, width/2);
+//        southEast.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
+//
+//    southWest = new SwerveModule(RobotMap.magnitudeMotor3, RobotMap.directionMotor3, RobotMap.encoder3ChannelA, RobotMap.encoder3ChannelB, -length/2, -width/2);
+//        southWest.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
+//
+//    northWest = new SwerveModule(RobotMap.magnitudeMotor4, RobotMap.directionMotor4, RobotMap.encoder4ChannelA, RobotMap.encoder4ChannelB, length/2, -width/2);
+//        northWest.setPID(deadAngle, Pvalue, Ivalue, Dvalue);
 
 
     public final CANLineSubsystem.SparkNeo FRONT_LEFT_WHEEL = new CANLineSubsystem.SparkNeo(SparkNeoDriveModule.DrivePosition.FRONT_LEFT);
