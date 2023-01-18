12f68450fc0bcf821a24cd1bd946b77dfba34c4e
2077 Robotics <kettlemorainerc@gmail.com>
2023-01-17 19:53:58 -0600

    Hall effect sensor testing

--- src/main/java/org/usfirst/frc/team2077/DriveStation.java
+++ src/main/java/org/usfirst/frc/team2077/DriveStation.java
@@ -68,7 +68,7 @@ public class DriveStation {
         JoystickButton pistonUp = new JoystickButton(secondary, 12);
         JoystickButton pistonDown = new JoystickButton(secondary, 16);
         JoystickButton toggleAirCompressor = new JoystickButton(secondary, 6);
-
+        JoystickButton calibrateRotation = new JoystickButton(secondary, 3);
 
         new LoadLauncher(hardware).bind(load);
         new StopLoading(hardware).bind(stopLoad);
@@ -77,6 +77,7 @@ public class DriveStation {
 
         new ChangePistonHeight(hardware, ChangePistonHeight.PistonDirection.UP).bind(pistonUp);
         new ChangePistonHeight(hardware, ChangePistonHeight.PistonDirection.DOWN).bind(pistonDown);
+        new CalibrateRotation(hardware).bind(calibrateRotation);
     }
 
     /** Normal (brighter/silver) joystick that supports rotation */
