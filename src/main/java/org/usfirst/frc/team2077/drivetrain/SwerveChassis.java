6b667715d18512bfe8baf63610b125b09bcec8b1
SquEdgy <dafman6@outlook.com>
2023-02-09 19:25:28 -0600

    Kinematics stuff

--- src/main/java/org/usfirst/frc/team2077/drivetrain/SwerveChassis.java
+++ src/main/java/org/usfirst/frc/team2077/drivetrain/SwerveChassis.java
@@ -1,8 +1,5 @@
 package org.usfirst.frc.team2077.drivetrain;
 
-
-import com.ctre.phoenix.led.TwinkleAnimation;
-import org.usfirst.frc.team2077.RobotHardware;
 import org.usfirst.frc.team2077.common.*;
 import org.usfirst.frc.team2077.common.drivetrain.*;
 import org.usfirst.frc.team2077.common.math.AccelerationLimits;
@@ -13,7 +10,6 @@ import org.usfirst.frc.team2077.subsystem.SwerveMotor;
 import java.util.*;
 import java.util.function.Supplier;
 
-import static javax.swing.SwingConstants.NORTH;
 import static org.usfirst.frc.team2077.common.VelocityDirection.*;
 
 public class SwerveChassis extends AbstractChassis<SwerveMotor> {
@@ -21,7 +17,7 @@ public class SwerveChassis extends AbstractChassis<SwerveMotor> {
     private static final double TRACK_WIDTH = 25.5; // inches
     private static final double WHEEL_RADIUS = 4.0; // inches
 
-//    private static final RotationMotor directionMotor = new RotationMotor();
+    //    private static final RotationMotor directionMotor = new RotationMotor();
 
     private final SwerveMath math;
     private final AngleSensor angleSensor;
@@ -36,73 +32,75 @@ public class SwerveChassis extends AbstractChassis<SwerveMotor> {
         return map;
     }
 
-    public SwerveChassis(HardwareRequirements<SwerveMotor, SwerveChassis> hardware, Supplier<Double> getSeconds) {
+    public SwerveChassis(
+          HardwareRequirements<SwerveMotor, SwerveChassis> hardware,
+          Supplier<Double> getSeconds
+    ) {
         super(buildDriveTrain(hardware), WHEELBASE, TRACK_WIDTH, WHEEL_RADIUS, getSeconds);
-//        super(buildDriveModule(pos -> hardware.getWheel(pos).motor), WHEELBASE, TRACK_WIDTH, WHEEL_RADIUS, getSeconds);
-//        rotationModules = buildDriveModule(hardware::getRotationModule);
+
         this.angleSensor = hardware.getAngleSensor();
         math = new SwerveMath(WHEELBASE, TRACK_WIDTH);
 
-        this.maximumRotation = 1;
-        this.maximumSpeed =  this.driveModules.values()
-                .stream()
-                .map( e -> {
-                    Double max = e.getMaximumSpeed();
-                    System.out.printf("[%s=%s]", e.getPosition(), max);
-                    return max;
-                })
-                .min(Comparator.naturalOrder())
-                .orElseThrow();;
+        this.maximumSpeed = this.driveModules.values()
+              .stream()
+              .map(e -> {
+                  Double max = e.getMaximumSpeed();
+                  System.out.printf("[%s=%s]", e.getPosition(), max);
+                  return max;
+              })
+              .min(Comparator.naturalOrder())
+              .orElseThrow();
+        ;
         System.out.println();
 
-//        maximumSpeed
-
-//        TestSwerve()
-        Map<WheelPosition, SwerveTargetValues> wheelTargets = math.targetsForVelocities(
-            Map.of(
-                FORWARD, 0.0, STRAFE, 0.0, ROTATION, 1.0
-            )
-        );
+        Map<WheelPosition, SwerveTargetValues> wheelTargets = math.targetsForVelocities(Map.of(
+              FORWARD,
+              0.0,
+              STRAFE,
+              0.0,
+              ROTATION,
+              1.0
+        ));
 
         Map<WheelPosition, TestSwerve> map = new EnumMap<>(WheelPosition.class);
-        wheelTargets.forEach(
-            (k, v) -> {
-                map.put(k, new TestSwerve(v.getAngle(), v.getMagnitude() * maximumSpeed));
-            }
-        );
+        wheelTargets.forEach((k, v) -> {
+            map.put(k, new TestSwerve(v.getAngle(), v.getMagnitude() * maximumSpeed));
+        });
 
-        maximumRotation = math.velocitiesForTargets(map).get(ROTATION);
+        maximumRotation = math.velocitiesForTargets(map)
+              .get(ROTATION);
 
-        this.minimumSpeed =  this.maximumSpeed * 0.1;
+        this.minimumSpeed = this.maximumSpeed * 0.1;
     }
 
     public SwerveChassis(HardwareRequirements<SwerveMotor, SwerveChassis> hardware) {
         this(hardware, Clock::getSeconds);
     }
 
-    @Override
-    protected void updatePosition() {
+    @Override protected void updatePosition() {
         velocitySet = getVelocityCalculated();
         velocityMeasured = math.velocitiesForTargets(driveModules);
 
-        positionSet.moveRelative(
-                velocitySet.get(FORWARD) * timeSinceLastUpdate,
-                velocitySet.get(STRAFE) * timeSinceLastUpdate,
-                velocitySet.get(ROTATION) * timeSinceLastUpdate
+        positionSet.moveRelative(velocitySet.get(FORWARD) * timeSinceLastUpdate,
+              velocitySet.get(STRAFE) * timeSinceLastUpdate,
+              velocitySet.get(ROTATION) * timeSinceLastUpdate
         );
-        positionMeasured.moveRelative(
-                velocityMeasured.get(FORWARD) * timeSinceLastUpdate,
-                velocityMeasured.get(STRAFE) * timeSinceLastUpdate,
-                velocityMeasured.get(ROTATION) * timeSinceLastUpdate
+        positionMeasured.moveRelative(velocityMeasured.get(FORWARD) * timeSinceLastUpdate,
+              velocityMeasured.get(STRAFE) * timeSinceLastUpdate,
+              velocityMeasured.get(ROTATION) * timeSinceLastUpdate
         );
     }
 
     public static WheelPosition LOGGED_POSITION = WheelPosition.FRONT_RIGHT;
-    @Override
-    protected void updateDriveModules() {
-        Map<WheelPosition, SwerveTargetValues> wheelTargets = math.targetsForVelocities(targetVelocity);
 
-        wheelTargets.forEach( (key, value) -> {
+    @Override protected void updateDriveModules() {
+        Map<WheelPosition, SwerveTargetValues> wheelTargets = math.targetsForVelocities(
+              targetVelocity,
+              maximumSpeed,
+              maximumRotation
+        );
+
+        wheelTargets.forEach((key, value) -> {
             SwerveMotor motor = this.driveModules.get(key);
 
             motor.setTargetAngle(value.getAngle());
@@ -110,14 +108,21 @@ public class SwerveChassis extends AbstractChassis<SwerveMotor> {
         });
     }
 
-    @Override
-    public void setVelocity(double north, double east, double clockwise, AccelerationLimits accelerationLimits) {
+    @Override public void setVelocity(
+          double north,
+          double east,
+          double clockwise,
+          AccelerationLimits accelerationLimits
+    ) {
         setVelocity(north, east, accelerationLimits);
         setRotation(clockwise, accelerationLimits);
     }
 
-    @Override
-    public void setVelocity(double north, double east, AccelerationLimits accelerationLimits) {
+    @Override public void setVelocity(
+          double north,
+          double east,
+          AccelerationLimits accelerationLimits
+    ) {
         targetVelocity.put(FORWARD, north);
         this.accelerationLimits.set(FORWARD, accelerationLimits.get(FORWARD));
 
@@ -125,65 +130,49 @@ public class SwerveChassis extends AbstractChassis<SwerveMotor> {
         this.accelerationLimits.set(STRAFE, accelerationLimits.get(STRAFE));
     }
 
-    @Override
-    public void setRotation(double clockwise, AccelerationLimits accelerationLimits) {
+    @Override public void setRotation(
+          double clockwise,
+          AccelerationLimits accelerationLimits
+    ) {
         targetVelocity.put(ROTATION, clockwise);
         this.accelerationLimits.set(ROTATION, accelerationLimits.get(ROTATION));
     }
 
-    private static class TestSwerve implements SwerveModule{
-
-        private final double wheelAngle;
-        private final double velocity;
-
-        TestSwerve(double wheelAngle, double velocity) {
-            this.wheelAngle = wheelAngle;
-            this.velocity = velocity;
+    private record TestSwerve(double wheelAngle, double velocity) implements SwerveModule {
+        @Override public double getVelocity() {
+            return this.velocity;
         }
 
-        @Override
-        public double getWheelAngle() {
-            return wheelAngle;
-        }
-
-        @Override
-        public double getVelocity() {
-            return velocity;
+        @Override public double getWheelAngle() {
+            return this.wheelAngle;
         }
 
         // region Ignored Things
-        @Override
-        public double getMaximumSpeed() {
+        @Override public double getMaximumSpeed() {
             return 0;
         }
 
-        @Override
-        public void setVelocity(double velocity) {
+        @Override public void setVelocity(double velocity) {
 
         }
 
-        @Override
-        public WheelPosition getWheelPosition() {
+        @Override public WheelPosition getWheelPosition() {
             return null;
         }
 
-        @Override
-        public double getDistance() {
+        @Override public double getDistance() {
             return 0;
         }
 
-        @Override
-        public void resetDistance() {
+        @Override public void resetDistance() {
 
         }
 
-        @Override
-        public void setTargetDegrees(double degrees) {
+        @Override public void setTargetDegrees(double degrees) {
 
         }
 
-        @Override
-        public void setTargetMagnitude(double magnitude) {
+        @Override public void setTargetMagnitude(double magnitude) {
 
         }
         // endregion
